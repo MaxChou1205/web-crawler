026f70fe38150139c214eebc16a5247d6680f5e1
MaxChou1205 <v2asault@yahoo.com.tw>
2025-05-16 20:58:24 +0800

    test

--- index.js
+++ index.js
@@ -338,6 +338,10 @@ mongoose.connect(db).then(async () => {
     }
   });
 
+  browser.userAgent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36"
+  );
+
   const messages = [];
   try {
     console.log("running a task every hour");
